b987ac89425bb4291bf24b5298b5d88d9559d683
HarleyAppleChoi <appleno56@gmail.com>
2020-11-20 13:26:04 +0800

    buggy pushup

--- account-linking/callback/appoloconfig.js
+++ account-linking/callback/appoloconfig.js
@@ -12,6 +12,9 @@
     .then(res => console.log(res.data));
  */
 
+var myList = document.querySelector('ul');
+
+
 const _userContents = `
 address
 dtag
@@ -79,85 +82,96 @@ bio
   }
   `;
 
-
-const query = `posts: post(
-  where: {
-    parent_id: {_is_null: true},
-    subspace: {_eq: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"}
-  }
-  order_by: { created: desc },
-  offset: 1,
-  limit: 11,
-) {
-  id
-  parent_id
-  subspace
-  created
-  hidden
-  last_edited
-  allows_comments
-  message
-  optional_data
-  media: medias {
-    uri
-    mime_type
-  }
-  poll {
-    question
-    end_date
-    allows_answer_edits
-    allows_multiple_answers
-    available_answers {
-      id: answer_id
-      text: answer_text
+  const newquery = ` {
+    post {
+      id
     }
-    user_answers {
-      answer
-      user {
-        address
-dtag
-moniker
-cover_pic
-profile_pic
-bio
+  }
+  `
+
+
+const query = `{
+  posts: post(where: {parent_id: {_is_null: true}, subspace: {_eq: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"}}, order_by: {created: desc}, offset: 1, limit: 11) {
+    id
+    parent_id
+    subspace
+    created
+    hidden
+    last_edited
+    allows_comments
+    message
+    optional_data
+    media: medias {
+      uri
+      mime_type
+    }
+    poll {
+      question
+      end_date
+      allows_answer_edits
+      allows_multiple_answers
+      available_answers {
+        id: answer_id
+        text: answer_text
+      }
+      user_answers {
+        answer
+        user {
+          address
+          dtag
+          moniker
+          cover_pic
+          profile_pic
+          bio
+        }
       }
     }
-  }
-  reactions {
-    user: owner {
-      address
-dtag
-moniker
-cover_pic
-profile_pic
-bio
+    reactions {
+      user: owner {
+        address
+        dtag
+        moniker
+        cover_pic
+        profile_pic
+        bio
+      }
+      value
     }
-    value
+    user: creator {
+      address
+      dtag
+      moniker
+      cover_pic
+      profile_pic
+      bio
+    }
+    comments: comments {
+      id
+    }
   }
-  user: creator {
-    address
-dtag
-moniker
-cover_pic
-profile_pic
-bio
-  }
-  comments: comments {
-    id
-  }
-}`
+}
+`
 
   //getPostDetails
-  fetch('https://m1k.gql.morpheus.desmos.network/v1/graphql', { 
+  fetch('https://gql.morpheus.desmos.network/v1/graphql', { 
     method: 'POST',   
     headers: { 'Content-Type': 'application/json' },   
     body: JSON.stringify({ 
       query: query}), 
     })   .then(res => res.json())
-    .then(res => console.log(res.data));
+    .then(data => 
+      {for(var i = 0;i< data.post;i++) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = 'posts:'+ post.message ;
+        listItem.innerHTML +=' created in: ' + post.created + '.';
+        listItem.innerHTML +=' Post by :' + post.address;
+        myList.appendChild(listItem);
+      }})
+    .then(json => console.log(json))
+    .catch(err => console.log(err.message, 'error'))
+    ;
 
-
-
+/*  */
 
   /// Represents the GQL query that should be used when wanting to subscribe
   /// to home events such as new post being added.
